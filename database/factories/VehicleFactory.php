ee92f74d5f07398cbf5bb42b5bf2da8cf2a97c16
filipjarek <filipjarek24@gmail.com>
2023-04-10 18:24:41 +0200

    Composer req pelmered/fake-car --dev and update VehicleFactory

--- database/factories/VehicleFactory.php
+++ database/factories/VehicleFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Provider\Fakecar;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Vehicle>
@@ -16,14 +17,14 @@ class VehicleFactory extends Factory
      */
     public function definition()
     {
-        $name = ['Audi A4','Bmw M2','Skoda Octavia','Opel Insignia'];
+        $this->faker->addProvider(new Fakecar($this->faker));
         $color = ['black','white','red','blue','pink','brown','silver','gold'];
         $capacity = ['1498','1998','2498','2998','998','1198',];
         $power = ['95','100','125','150','170','163','204','225','98','75','180','145','175','195',];
 
         return [
 
-            'name' => $name[random_int(0,2)],
+            'name' => $this->faker->vehicle(),
             'category' => 'car',
             'color' => $color[random_int(0,4)],
             'year' => random_int(1999,2023),
